64c7235fc370c4fd293719894802073582d74d22
Naah-Cindy <aformiehcindy@gmail.com>
2025-07-05 05:06:22 +0100

    Set Cloudinary resource_type to raw for encrypted files

--- securefile/settings.py
+++ securefile/settings.py
@@ -194,6 +194,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': config('CLOUDINARY_API_KEY'),
     'API_SECRET': config('CLOUDINARY_API_SECRET'),
+    'RESOURCE_TYPE': 'raw',
 }
 
 # Use Cloudinary for media files
